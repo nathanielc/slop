9adabaa4ee1d8fba67361b11b8a1088cf22c91cf
Nathaniel Cook <nvcook42@gmail.com>
2020-09-06 17:31:08 -0600

    use lalrpop parser generagor

---
+++ src/ast.rs
@@ -0,0 +1,6 @@
+#[derive(Debug,PartialEq)]
+pub enum Operand {
+    Ingredient(String),
+    UnaryOp( Box<Operand>, String),
+    BinaryOp(Box<Operand>, Box<Operand>, String),
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,73 @@
-extern crate peg;
 
-mod parser;
+mod ast;
+use ast::Operand;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub rcp); // synthesized by LALRPOP
+
+#[cfg(test)]
+mod test{
+    use super::*;
+    fn test_parse(src:&str, ast:ast::Operand) {
+        assert_eq!(
+            ast,
+            rcp::RecipeParser::new().parse(src).unwrap(),
+        )
+    }
+    #[test]
+    fn ingredient() {
+        test_parse(
+            "<*sugar 1 cup>",
+            Operand::Ingredient("sugar 1 cup".to_owned()),
+        );
+    }
+    #[test]
+    fn unary() {
+        test_parse(
+            "<*sugar 1 cup =pulverize>",
+            Operand::UnaryOp(
+                Box::new(Operand::Ingredient("sugar 1 cup".to_owned())),
+                "pulverize".to_string(),
+            ),
+        );
+    }
+    #[test]
+    fn binary() {
+        test_parse(
+            "<*sugar 1 cup *milk 3 cups #boil and stir>",
+            Operand::BinaryOp(
+                Box::new(Operand::Ingredient("sugar 1 cup".to_owned())),
+                Box::new(Operand::Ingredient("milk 3 cups".to_owned())),
+                "boil and stir".to_string(),
+            ),
+        );
+    }
+    #[test]
+    fn simple() {
+        test_parse(
+            "<
+*water 6 cups =boil
+*macarroni noodles 2 cups #add =drain
+*butter 1/4 cup #stir until melted
+*milk 1/3 cup #stir
+*dried cheese one pouch #stir until well mixed
+>",
+
+            Operand::BinaryOp(
+                Box::new(Operand::BinaryOp(
+                        Box::new(Operand::BinaryOp(
+                               Box::new( Operand::UnaryOp(
+                                    Box::new( Operand::BinaryOp(
+                                             Box::new( Operand::UnaryOp( Operand::Ingredient("water 6 cups"), "boil")),
+                                             Ingredient("macarroni noodles 2 cups"),
+                                             "add")),
+                                    "drain")),
+                                Operand::Ingredient("butter 1/4 cup"), "stir until melted")), Operand::Ingredient("milk 1/3 cup"), "stir")), Operand::Ingredient("dried cheese one pouch"), "stir until well mixed"),
+        );
+    }
 }
