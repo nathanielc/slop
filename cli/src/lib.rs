c38b1ebe8116b1d6d746025d2b2096c1c456a4a3
Nathaniel Cook <nvcook42@gmail.com>
2023-10-15 12:25:04 -0600

    feat: add render cmd to cli

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use anyhow::Result;
 use clap::{command, Args, Parser, Subcommand};
-use tokio::fs;
+use tokio::{fs, io::AsyncWriteExt};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -19,6 +19,8 @@ enum Command {
     Check(CheckOpts),
     /// Check if a slop file is valid.
     Title(TitleOpts),
+    /// Render recipe to an svg file
+    Render(RenderOpts),
 }
 
 #[derive(Args, Debug)]
@@ -41,6 +43,17 @@ struct TitleOpts {
     #[arg()]
     file: PathBuf,
 }
+
+#[derive(Args, Debug)]
+struct RenderOpts {
+    /// Path to slop file
+    #[arg()]
+    in_file: PathBuf,
+    /// Path to output svg file
+    #[arg()]
+    out_file: PathBuf,
+}
+
 pub async fn run() -> Result<()> {
     let args = Cli::parse();
     match args.command {
@@ -59,6 +72,15 @@ pub async fn run() -> Result<()> {
             let source = fs::read_to_string(opts.file).await?;
             let ast = slop::parse(&source)?;
             println!("{}", ast.recipes[0].title.as_ref().unwrap());
+            Ok(())
+        }
+        Command::Render(opts) => {
+            let source = fs::read_to_string(opts.in_file).await?;
+            let ast = slop::parse(&source)?;
+            let sem = slop::semantic::convert_source_file(ast);
+            let svg = slop::svg::to_svg(&sem);
+            let mut f = fs::File::create(opts.out_file).await?;
+            f.write_all(&svg).await?;
             Ok(())
         }
     }
